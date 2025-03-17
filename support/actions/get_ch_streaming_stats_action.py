3fc575eb92a7085c7bc2bc753cec56cd2f25bc65
Aaron Cameron <aaron.cameron@workiva.com>
2025-03-17 10:02:08 -0600

    Streaming stats support action retrieves latest discovery details

--- support/actions/get_ch_streaming_stats_action.py
+++ support/actions/get_ch_streaming_stats_action.py
@@ -9,7 +9,9 @@ class GetChStreamingStatsAction(BaseAction):
                 "MIN(created_date) AS earliest_created_date, " \
                 "MIN(timepoint) AS earliest_timepoint, " \
                 "MAX(created_date) AS latest_created_date, " \
-                "MAX(timepoint) AS latest_timepoint " \
+                "MAX(timepoint) AS latest_timepoint, " \
+                "(SELECT MAX(stream_timepoint) FROM filings WHERE stream_timepoint IS NOT NULL) AS latest_discovery_timepoint, " \
+                "(SELECT MAX(discovered_date) FROM filings WHERE stream_timepoint IS NOT NULL) AS latest_discovery_date " \
                 "FROM stream_events"
         cursor.execute(query)
         stats = BaseAction.collect_stats(cursor)
